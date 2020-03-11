b2345e275d7ef3468c0ac12c1d83c620b689e08a
krikaz <pascal.bou@protonmail.com>
2020-03-11 21:46:54 +0100

    finished, last name sorter not working

--- Students/Model/StudentController.swift
+++ Students/Model/StudentController.swift
@@ -8,10 +8,71 @@
 
 import Foundation
 
+enum TrackType: Int {
+    case none
+    case iOS
+    case Web
+    case UX
+}
+
+enum SortOptions: Int {
+    case firstName
+    case lastName
+}
+
 class StudentController {
+    
+    private var students: [Student] = []
     
     private var persistentFileURL: URL? {
         guard let filePath = Bundle.main.path(forResource: "students", ofType: "json") else { return nil }
         return URL(fileURLWithPath: filePath)
+    }
+    
+    func loadFromPersistentStore(completion: @escaping ([Student]?, Error?) -> ()) {
+        let bgQueue = DispatchQueue(label: "studentQueue", attributes: .concurrent)
+        
+        bgQueue.async {
+            let fm = FileManager.default
+            guard let url = self.persistentFileURL, fm.fileExists(atPath: url.path) else { return }
+            
+            do {
+                let data = try Data(contentsOf: url)
+                // convert data to Student objects
+                let decoder = JSONDecoder()
+                
+                // store those in students array
+                self.students = try decoder.decode([Student].self, from: data)
+                
+                completion(self.students, nil)
+                
+            } catch {
+                print("Error loading student data: \(error)")
+                completion(nil, error)
+            }
+        }
+    }
+    
+    func filter(with trackType: TrackType, sortedBy sorter: SortOptions, completion: @escaping ([Student]) -> Void) {
+        var updatedStudents: [Student]
+        
+        switch trackType {
+        case .iOS:
+            updatedStudents = students.filter { $0.course == "iOS" }
+        case .Web:
+            updatedStudents = students.filter { $0.course == "Web" }
+        case .UX:
+            updatedStudents = students.filter { $0.course == "UX" }
+        default:
+            updatedStudents = students
+        }
+        
+        if sorter == .firstName {
+            updatedStudents = updatedStudents.sorted { $0.firstName < $1.firstName }
+        } else {
+            updatedStudents = updatedStudents.sorted { $0.lastName < $1.lastName }
+        }
+        
+        completion(updatedStudents)
     }
 }
--- Students/View Controllers/StudentsViewController.swift
+++ Students/View Controllers/StudentsViewController.swift
@@ -17,37 +17,67 @@ class StudentsViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     // MARK: - Properties
+    
+    private let studentController = StudentController()
+    private var filteredAndSortedStudents: [Student] = [] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.dataSource = self
+        
+        studentController.loadFromPersistentStore { (students, error) in
+            if let error = error {
+                NSLog("Error loading students: \(error)")
+                return
+            }
+            
+            DispatchQueue.main.async {
+                if let students = students {
+                    self.filteredAndSortedStudents = students
+                }
+            }
+        }
+        
     }
     
     // MARK: - Action Handlers
     
     @IBAction func sort(_ sender: UISegmentedControl) {
+        updateDataSource()
     }
     
     @IBAction func filter(_ sender: UISegmentedControl) {
+        updateDataSource()
     }
     
     // MARK: - Private
     
     private func updateDataSource() {
+        let filter = TrackType(rawValue: filterSelector.selectedSegmentIndex) ??  .none
+        let sort = SortOptions(rawValue: filterSelector.selectedSegmentIndex) ?? .firstName
         
+        studentController.filter(with: filter, sortedBy: sort) { (students) in
+            self.filteredAndSortedStudents = students
+        }
     }
 }
 
 extension StudentsViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return filteredAndSortedStudents.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "StudentCell", for: indexPath)
         
-        // Configure cell
+        let oneStudent = filteredAndSortedStudents[indexPath.row]
+        cell.textLabel?.text = oneStudent.name
+        cell.detailTextLabel?.text = oneStudent.course
         
         return cell
     }
